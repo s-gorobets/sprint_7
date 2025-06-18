57aae088d04937a0044b7203078c6eec8353e69d
Sergei Gorobets <sergei.gorobets@vk.team>
2025-06-18 19:51:12 +0300

    Оставлен только README.md

--- urls.py
+++
@@ -1,6 +0,0 @@
-BASE_URL = 'https://qa-scooter.praktikum-services.ru'
-CREAT_CURIER_URL = 'https://qa-scooter.praktikum-services.ru/api/v1/courier'
-LOGIN_CURIER_URL = 'https://qa-scooter.praktikum-services.ru/api/v1/courier/login'
-CREAT_ORDER_URL = 'https://qa-scooter.praktikum-services.ru/api/v1/orders'
-LIST_ORDERS_URL = 'https://qa-scooter.praktikum-services.ru/api/v1/orders'
-
